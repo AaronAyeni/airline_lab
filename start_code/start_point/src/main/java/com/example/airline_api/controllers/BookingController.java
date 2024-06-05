d1fc7afce3ae296c9179e454bebb7caf02b86f78
Aaron Ayeni <aaronayeni135@gmail.com>
2024-06-06 00:42:06 +0100

    completed controllers for mvp

--- start_code/start_point/src/main/java/com/example/airline_api/controllers/BookingController.java
+++ start_code/start_point/src/main/java/com/example/airline_api/controllers/BookingController.java
@@ -1,6 +1,9 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Booking;
+import com.example.airline_api.services.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +12,10 @@ import java.util.List;
 @RequestMapping("/bookings")
 public class BookingController {
 
-    // TODO: Display all bookings
+    @Autowired
+    private BookingService bookingService;
+
+
     @GetMapping
     public ResponseEntity<List<Booking>> getAllBookings(){
         return null;
@@ -17,8 +23,10 @@ public class BookingController {
 
     // TODO: Add details of a new booking
     @PostMapping(value = "/{id}")
-    public ResponseEntity<Booking> addNewBooking(){
-        return null;
+    public ResponseEntity<Booking> addNewBooking(@RequestParam Long passengerId, @RequestParam Long flightId,
+                                                 @RequestParam int seatNumber, @RequestParam String mealPreference){
+        Booking booking = bookingService.bookFlight(passengerId, flightId, seatNumber, mealPreference);
+        return new ResponseEntity<>(booking, HttpStatus.CREATED);
     }
 
     // TODO: Extension - Update passenger meal preference
--- start_code/start_point/src/main/java/com/example/airline_api/controllers/PassengerController.java
+++ start_code/start_point/src/main/java/com/example/airline_api/controllers/PassengerController.java
@@ -1,6 +1,9 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Passenger;
+import com.example.airline_api.services.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,22 +13,32 @@ import java.util.List;
 @RequestMapping("/passengers")
 public class PassengerController {
 
-    // TODO: Display details of all passengers
+    @Autowired
+    PassengerService passengerService;
+
+
     @GetMapping
     public ResponseEntity<List<Passenger>> getAllPassengers(){
-        return null;
+        List<Passenger> passengers = passengerService.getAllPassengers();
+        return new ResponseEntity<>(passengers, HttpStatus.OK);
     }
 
-    // TODO: Display specific passenger details
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Passenger> getPassengerById(){
-        return null;
+    public ResponseEntity<Passenger> getPassengerById(@PathVariable Long id){
+        Passenger passenger = passengerService.getPassengerById(id);
+        if (passenger != null) {
+            return new ResponseEntity<>(passenger, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    // TODO: Add a new passenger
+
     @PostMapping
-    public ResponseEntity<Passenger> addNewPassenger(){
-        return null;
+    public ResponseEntity<Passenger> addNewPassenger(@RequestBody Passenger passenger){
+        Passenger savedPassenger = passengerService.addPassenger(passenger);
+        return new ResponseEntity<>(savedPassenger, HttpStatus.CREATED);
     }
 
 }

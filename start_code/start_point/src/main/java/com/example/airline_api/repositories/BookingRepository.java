8478514565c4371de83519c3f9cbeceb4d702f16
Aaron Ayeni <aaronayeni135@gmail.com>
2024-06-05 23:09:52 +0100

    completed annotations for booking,passengers and flight classes and joined them

---
+++ start_code/start_point/src/main/java/com/example/airline_api/repositories/BookingRepository.java
@@ -0,0 +1,10 @@
+package com.example.airline_api.repositories;
+
+import com.example.airline_api.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+}
---
+++ start_code/start_point/src/main/java/com/example/airline_api/repositories/PassengerRepository.java
@@ -0,0 +1,9 @@
+package com.example.airline_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerRepository extends JpaRepository {
+
+}
